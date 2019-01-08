b11f1fe5de72ec722d7af182314355145b431334
Thomas Bahen <tombahen@gmail.com>
2019-01-08 11:08:04 -0500

    Initial Commit

---
+++ ShopifyMobileApplication/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  ShopifyMobileApplication
+//
+//  Created by Tom  Bahen on 2019-01-08.
+//  Copyright © 2019 Tom  Bahen. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
